c80c76702d6055ff668e1c939e8e12937deabebf
bastimeyer <mail@bastimeyer.de>
2019-04-23 03:41:08 +0200

    native classes: stream model

--- src/app/data/models/stream/model.js
+++ src/app/data/models/stream/model.js
@@ -1,13 +1,15 @@
-import { get, set, computed, observer } from "@ember/object";
+import { set, computed } from "@ember/object";
 import { alias, not } from "@ember/object/computed";
 import { run } from "@ember/runloop";
 import { inject as service } from "@ember/service";
 import attr from "ember-data/attr";
 import Model from "ember-data/model";
 import { belongsTo } from "ember-data/relationships";
+import { observes } from "@ember-decorators/object";
 import { twitch as twitchConfig } from "config";
 import qualitiesStreamlink from "./-qualities/streamlink";
 import qualitiesLivestreamer from "./-qualities/livestreamer";
+import { name } from "utils/decorators";
 
 
 const {
@@ -25,6 +27,9 @@ const STATUS_WATCHING = 3;
 const STATUS_COMPLETED = 4;
 
 
+const { hasOwnProperty } = {};
+
+
 const createPresetObj = ( presets, preset ) => {
 	presets[ preset.id ] = preset;
 	return presets;
@@ -33,19 +38,17 @@ const qualitiesByIdStreamlink = qualitiesStreamlink.reduce( createPresetObj, {} 
 const qualitiesByIdLivestreamer = qualitiesLivestreamer.reduce( createPresetObj, {} );
 
 
-function computedStatus( status ) {
-	return computed( "status", {
-		get() {
-			return get( this, "status" ) === status;
-		},
-		set( value ) {
-			if ( value ) {
-				set( this, "status", status );
-				return true;
-			}
+const computedStatus = status => computed( "status", {
+	get() {
+		return this.status === status;
+	},
+	set( value ) {
+		if ( value ) {
+			set( this, "status", status );
+			return true;
 		}
-	});
-}
+	}
+});
 
 
 export {
@@ -55,162 +58,170 @@ export {
 };
 
 
-/**
- * @class Stream
- */
-export default Model.extend({
-	/** @property {TwitchStream} stream */
-	stream: belongsTo( "twitchStream", { async: false } ),
-	/** @property {TwitchChannel} channel */
-	channel: belongsTo( "twitchChannel", { async: false } ),
-	quality: attr( "string" ),
-	chat_open: attr( "boolean" ),
-	started: attr( "date" ),
+@name( "Stream" )
+export default class Stream extends Model {
+	/** @type {AuthService} */
+	@service auth;
+	/** @type {SettingsService} */
+	@service settings;
+	/** @type {StreamingService} */
+	@service streaming;
+
+	/** @type {TwitchStream} */
+	@belongsTo( "twitch-stream", { async: false } )
+	stream;
+	/** @type {TwitchChannel} */
+	@belongsTo( "twitch-channel", { async: false } )
+	channel;
+	@attr( "string" )
+	quality;
+	@attr( "boolean" )
+	chat_open;
+	@attr( "date" )
+	started;
 
 
 	// let Streamlink/Livestreamer use the GUI's client-id
-	clientID: `Client-ID=${clientId}`,
+	clientID = `Client-ID=${clientId}`;
 
 	// passthrough type (twitch streams are HLS)
-	playerInputPassthrough: "hls",
+	playerInputPassthrough = "hls";
 
-	/** @property {String} status */
-	status: STATUS_PREPARING,
+	/** @type {boolean} */
+	strictQuality = false;
 
-	/** @property {ChildProcess} spawn */
-	spawn: null,
+	/** @type {number} */
+	status = STATUS_PREPARING;
 
-	/** @property {Error} error */
-	error: null,
-	warning: false,
+	/** @type {ChildProcess} */
+	spawn = null;
 
-	/** @property {Object[]} log */
-	log: null,
-	showLog: false,
+	/** @type {Error} */
+	error = null;
+	warning = false;
 
+	/** @type {Object[]} */
+	log = null;
+	showLog = false;
 
-	auth: service(),
-	settings: service(),
-	streaming: service(),
-
-
-	session: alias( "auth.session" ),
-
-
-	isPreparing: computedStatus( STATUS_PREPARING ),
-	isAborted: computedStatus( STATUS_ABORTED ),
-	isLaunching: computedStatus( STATUS_LAUNCHING ),
-	isWatching: computedStatus( STATUS_WATCHING ),
-	isCompleted: computedStatus( STATUS_COMPLETED ),
-
-	hasEnded: computed( "status", "error", function() {
-		const status = get( this, "status" );
-		const error = get( this, "error" );
-
-		return !!error || status === STATUS_ABORTED || status === STATUS_COMPLETED;
-	}),
+	/** @type {Auth} */
+	@alias( "auth.session" )
+	session;
 
 
-	isLivestreamer: not( "isStreamlink" ),
-	isStreamlink: alias( "settings.streaming.isStreamlink" ),
+	@computedStatus( STATUS_PREPARING )
+	isPreparing;
+	@computedStatus( STATUS_ABORTED )
+	isAborted;
+	@computedStatus( STATUS_LAUNCHING )
+	isLaunching;
+	@computedStatus( STATUS_WATCHING )
+	isWatching;
+	@computedStatus( STATUS_COMPLETED )
+	isCompleted;
+
+	@computed( "status", "error" )
+	get hasEnded() {
+		return !!this.error
+		    || this.status === STATUS_ABORTED
+		    || this.status === STATUS_COMPLETED;
+	}
+
+
+	@not( "isStreamlink" )
+	isLivestreamer;
+	@alias( "settings.content.streaming.isStreamlink" )
+	isStreamlink;
 
 
 	get customParameters() {
-		const provider  = get( this, "settings.streaming.provider" );
-		const providers = get( this, "settings.streaming.providers" );
+		const { provider, providers } = this.settings.content.streaming;
 
-		return get( providers, `${provider}.params` ) || "";
-	},
+		return hasOwnProperty.call( providers, provider )
+			? providers[ provider ].params || ""
+			: "";
+	}
 
 
 	kill() {
 		if ( this.spawn ) {
 			this.spawn.kill( "SIGTERM" );
 		}
-	},
+	}
 
 	pushLog( type, line ) {
-		get( this, "log" ).pushObject({ type, line });
-	},
+		this.log.pushObject({ type, line });
+	}
 
 
-	qualityObserver: observer( "quality", function() {
+	@observes( "quality" )
+	qualityObserver() {
 		// the StreamingService knows that it has to spawn a new child process
 		this.kill();
-	}),
+	}
 
 
 	// get the default quality object of the selected quality and streaming provider
-	streamQualityPreset: computed( "quality", "isStreamlink", function() {
-		const quality = get( this, "quality" );
-		const isStreamlink = get( this, "isStreamlink" );
-		const qualities = isStreamlink
+	@computed( "quality", "isStreamlink" )
+	get streamQualityPreset() {
+		const qualities = this.isStreamlink
 			? qualitiesByIdStreamlink
 			: qualitiesByIdLivestreamer;
 
-		return qualities[ quality ]
+		return qualities[ this.quality ]
 		    || qualities[ "source" ];
-	}),
+	}
 
 	// get the --stream-sorting-excludes parameter value (Streamlink)
-	streamQualitiesExclude: computed(
-		"streamQualityPreset",
-		"settings.streaming.qualities",
-		function() {
-			const { id, exclude } = get( this, "streamQualityPreset" );
-			const custom = get( this, "settings.streaming.qualities" ).toJSON();
+	@computed( "streamQualityPreset", "settings.content.streaming.qualities" )
+	get streamQualitiesExclude() {
+		const { id, exclude } = this.streamQualityPreset;
+		const custom = this.settings.content.streaming.qualities.toJSON();
 
-			return custom.hasOwnProperty( id )
-			    && String( custom[ id ][ "exclude" ] || "" ).trim().length > 0
-				? custom[ id ][ "exclude" ]
-				: exclude;
-		}
-	),
+		return hasOwnProperty.call( custom, id )
+		    && String( custom[ id ][ "exclude" ] || "" ).trim().length > 0
+			? custom[ id ][ "exclude" ]
+			: exclude;
+	}
 
 	// get the stream quality selection (for both Streamlink and Livestreamer)
-	streamQuality: computed(
+	@computed(
 		"streamQualityPreset",
-		"settings.streaming.qualities",
-		"settings.streaming.qualitiesOld",
-		"isStreamlink",
-		function() {
-			const { id, quality } = get( this, "streamQualityPreset" );
+		"settings.content.streaming.qualities",
+		"settings.content.streaming.qualitiesOld",
+		"isStreamlink"
+	)
+	get streamQuality() {
+		const { id, quality } = this.streamQualityPreset;
 
-			if ( get( this, "isStreamlink" ) ) {
-				const custom = get( this, "settings.streaming.qualities" ).toJSON();
+		if ( this.isStreamlink ) {
+			const custom = this.settings.content.streaming.qualities.toJSON();
 
-				// get the quality attribute of the streamlink quality fragment
-				return custom.hasOwnProperty( id )
-				    && String( custom[ id ][ "quality" ] || "" ).trim().length > 0
-					? custom[ id ][ "quality" ]
-					: quality;
+			// get the quality attribute of the streamlink quality fragment
+			return hasOwnProperty.call( custom, id )
+			    && String( custom[ id ][ "quality" ] || "" ).trim().length > 0
+				? custom[ id ][ "quality" ]
+				: quality;
 
-			} else {
-				const custom = get( this, "settings.streaming.qualitiesOld" ).toJSON();
+		} else {
+			const custom = this.settings.content.streaming.qualitiesOld.toJSON();
 
-				// get the value of the livestreamer quality attribute
-				return custom.hasOwnProperty( id )
-				    && String( custom[ id ] || "" ).trim().length > 0
-					? custom[ id ]
-					: quality;
-			}
+			// get the value of the livestreamer quality attribute
+			return hasOwnProperty.call( custom, id )
+			    && String( custom[ id ] || "" ).trim().length > 0
+				? custom[ id ]
+				: quality;
 		}
-	),
+	}
 
-	streamUrl: computed( "channel.name", function() {
-		const channel = get( this, "channel.name" );
-
-		return twitchStreamUrl.replace( "{channel}", channel );
-	}),
+	@computed( "channel.name" )
+	get streamUrl() {
+		return twitchStreamUrl.replace( "{channel}", this.channel.name );
+	}
 
 	async destroyStream() {
 		// TODO: wait for ED fix that unloads records after destroying them
 		await run( () => this.destroyRecord() );
 		run( () => this.unloadRecord() );
 	}
-
-}).reopenClass({
-
-	toString() { return "Stream"; }
-
-});
+}
